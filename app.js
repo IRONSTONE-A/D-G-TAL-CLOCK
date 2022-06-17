781eae4a19250e5b3f639513c3a606fb0a303601
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-06-17 21:54:34 +0300

    js was created

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+
+//! HTML de yazacağım tag in class ını seçiyoruz...
 const time = document.querySelector('.time');
 
+//! fonksiyonumuzu oluşturuyoruz...
+function digitalClock() {
+    let dateTime = new Date();
+    let clock = dateTime.toLocaleTimeString();
+    time.innerHTML = clock;
+}
+
+//! kaç milisaniyede çalışacağını belirliyoruz...
+setInterval(digitalClock, 1000);
